ef73a0e0aff9d90c273638d807c43834c5fd0c4f
Gulnawaj <gulnawaj001646@gmail.com>
2026-06-30 09:54:54 +0530

    stl queue

---
+++ Arrays/code36.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    while(!s.empty()){
+        cout<<s.front()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+}
