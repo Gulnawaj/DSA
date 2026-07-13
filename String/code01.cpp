696af0028d44fa93d6ca7af6c5525a063f2699ff
Gulnawaj <gulnawaj001646@gmail.com>
2026-07-13 15:16:54 +0530

    basic

---
+++ String/code01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char arr[10];
+    for(int i = 0 ; i<10 ; i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+     for(int i = 0 ; i<10 ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
