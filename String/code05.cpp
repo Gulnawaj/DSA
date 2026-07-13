ee97345f057d4b414ec1acc1d74ad41d704da928
Gulnawaj <gulnawaj001646@gmail.com>
2026-07-13 15:59:15 +0530

    basic5

---
+++ String/code05.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+     string s1 = "gul" , s2 = "nawaj";
+     string s3 = s1+s2;
+     cout<<s3<<endl;
+     string s4 = s1.append(s2);
+     cout<<s4;
+}
