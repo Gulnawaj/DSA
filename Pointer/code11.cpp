85db35f307cede66857b317f2cfb2bed80c1984e
Gulnawaj <gulnawaj001646@gmail.com>
2026-07-26 12:11:38 +0530

    pointers in vectors

---
+++ Pointer/code11.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//pointers in vectors
+int main()
+{
+    vector<int> v = {10,20,30};
+    vector<int> *ptr = &v;
+    cout << (*ptr)[0];
+}
