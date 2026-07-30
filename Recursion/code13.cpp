3367452297ccb615f360e6449eee6b7b40094e30
Gulnawaj <gulnawaj001646@gmail.com>
2026-07-30 23:54:36 +0530

    gcd of two no.

---
+++ Recursion/code13.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+//gcd of two number 
+int gcd(int a, int b) {
+    int ans = 1;
+
+    for (int i = 1; i <= min(a, b); i++) {
+        if (a % i == 0 && b % i == 0)
+            ans = i;
+    }
+
+    return ans;
+}
+
+int main() {
+    int a , b;
+    cin>>a>>b;
+    cout << gcd(a, b);
+}
