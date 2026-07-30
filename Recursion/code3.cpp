d821b979f198886a09cda50d371db899069a0192
Gulnawaj <gulnawaj001646@gmail.com>
2026-07-30 13:44:42 +0530

    fibo series

---
+++ Recursion/code3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void printFib(int a, int b, int n) {
+    if (n == 0)
+        return;
+
+    cout << a << " ";
+
+    printFib(b, a + b, n - 1);
+}
+
+int main() {
+    int n;
+    cout<<"enter value ";
+    cin>>n;
+    printFib(0, 1, n);
+    return 0;
+}
